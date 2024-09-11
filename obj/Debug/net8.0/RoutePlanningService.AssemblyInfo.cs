0bd5320ac81d6a81713e758b1bc3ca09ec02ac90
felixojiambo <felixojiamboe@gmail.com>
2024-09-11 09:45:07 +0300

    EF Core database context that manages trip data.

--- obj/Debug/net8.0/RoutePlanningService.AssemblyInfo.cs
+++ obj/Debug/net8.0/RoutePlanningService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RoutePlanningService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a34c86c1aa721528a36d85545abfadbf179b6844")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f68ffa4ebd60e36cfe1bc9c9500a5cde89f18df")]
 [assembly: System.Reflection.AssemblyProductAttribute("RoutePlanningService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RoutePlanningService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
